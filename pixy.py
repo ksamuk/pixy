0a5024200655944e4f28b00e4b497cee514cb815
ksamuk <ksamuk@gmail.com>
2019-07-22 14:02:55 -0400

    filtration and parsing sections updated

--- pixy.py
+++ pixy.py
@@ -10,6 +10,8 @@ import numcodecs
 import numpy as np
 import sys
 import os
+import re
+import operator
 import matplotlib.pyplot as plt
 from itertools import combinations
 from collections import Counter
@@ -17,7 +19,7 @@ from tqdm import tqdm
 import argparse
 
 
-# In[17]:
+# In[2]:
 
 
 # parse the command line arguements
@@ -44,14 +46,14 @@ parser.add_argument('--vcf', type=str, nargs='?', help='Path to the input VCF', 
 parser.add_argument('--zarr', type=str, nargs='?', help='Folder in which to build the Zarr database', required=True)
 parser.add_argument('--populations', type=str, nargs='?', help='Path to the populations file')
 parser.add_argument('--window_size', type=int, nargs='?', help='Window size in base pairs over which to calculate pi/dxy')
-parser.add_argument('--snp_filter_expression', type=str, nargs='?', help='A comma separated list of filters (e.g. DP>=10,GQ>=20) to apply to SNPs', required=True)
+parser.add_argument('--filter_expression', type=str, nargs='?', help='A comma separated list of filters (e.g. DP>=10,GQ>=20) to apply to SNPs', required=True)
 parser.add_argument('--invariant_filter_expression', type=str, nargs='?', help='A comma separated list of filters (e.g. DP>=10,RGQ>=20) to apply to invariant sites', required=True)
 parser.add_argument('--outfile', type=str, nargs='?', help='Path to the output file')
 
 #parser.print_help()
 
 # pull out varizbles from the parsed args
-args = parser.parse_args('--stats pi_dxy --vcf test.vcf --zarr test/path/ --populations path/to/popfile.txt --snp_filter_expression DP>=10,GQ>=20,FS>2 --invariant_filter_expression DP>=10,RGQ>=20 --outfile pixy_out.txt'.split())
+args = parser.parse_args('--stats pi_dxy --vcf test.vcf --zarr test/path/ --populations path/to/popfile.txt --filter_expression DP>=10,GQ>=20,RGQ>=20 --invariant_filter_expression DP>=10,RGQ>=20 --outfile pixy_out.txt'.split())
 # sim_args = parser.parse_args()
 
 if (args.populations is None) and ((args.stats == 'dxy' or args.stats == 'pi_dxy')):
@@ -63,7 +65,7 @@ if (args.populations is None) and ((args.stats == 'dxy' or args.stats == 'pi_dxy
 
 print(args)
 
-print(args.invariant_filter_expression)
+print(args.filter_expression)
 
 
 # In[4]:
@@ -96,31 +98,84 @@ callset = zarr.open_group(zarr_path, mode='r')
 #callset.tree(expand=True)
 
 
-# In[6]:
+# In[14]:
 
 
-# filtering the data 
+# parse the filtration expression and build the boolean filter array
 
-# - extracting the filtration values from the filter strings
-# - applying the filter to the whole dataset
-# - also filter out biallelic snps
-# - check to see there is any data left
-# - print warning for low data* think more about this
+# define an operator dictionary for parsing the operator strings
+ops = { "<": operator.lt, "<=": operator.le, ">": operator.gt, ">=": operator.ge, "==": operator.eq}
+
+filters = []
+
+# split the filtration expressions by commas
+# parse out each component
+# apply the filter
+# TBD: handle cases where the specified filter isn't in the callset
+for x in args.filter_expression.split(","):
+    stat = re.sub("[^A-Za-z]+", "", x)
+    value = int(re.sub("[^0-9]+", "", x))
+    compare = re.sub("[A-Za-z0-9]+", "", x)
+    
+    if type(filters) is list:
+        filters = ops[compare](callset[chromosome + '/calldata/' + stat][:], value)
+    else:
+        if stat == 'GQ':
+            GQ_filter = ops[compare](callset[chromosome + '/calldata/' + stat][:], value)
+        elif stat == 'RGQ':
+            RGQ_filter = ops[compare](callset[chromosome + '/calldata/' + stat][:], value)
+        else :
+            filters = np.logical_and(filters, ops[compare](callset[chromosome + '/calldata/' + stat][:], value))
+
+# check if GQ and RQG exist
+# if they both exist, perform a logical OR and join them into the filter
+# otherwise, perform a logical AND to join either one into the filter
+
+try:
+    GQ_filter
+except NameError:
+    GQ_exists = False
+else:
+    GQ_exists = True
+    
+try:
+    RGQ_filter
+except NameError:
+    RGQ_exists = False
+else:
+    RGQ_exists = True
+    
+if GQ_exists & RGQ_exists:
+    filters = np.logical_and(filters, np.logical_or(GQ_filter, RGQ_filter))
+elif GQ_exists:
+    filters = np.logical_and(filters, GQ_filter)
+elif RGQ_exists:
+    filters = np.logical_and(filters, RGQ_filter)
+    
+# finally, invert the whole array 
+# this is for convenience/brevity in the next section
+
+filters = np.invert(filters)
+
+
+# In[15]:
+
+
+# applying the filter to the data
+# all the filters are in a boolean array ('filters') 
+
+# TBD: check to see there is any data left
+# TBD: print warning for low data* think more about this
 
 # the genotype calls
 # recode the gt matrix as a Dask array (saves memory)
 gt_dask = allel.GenotypeDaskArray(callset[chromosome + '/calldata/GT'])
 
 # create a packed genotype array 
-# this is a compact array with dims snps x samples
-# genotypes are represented by single byte codes
+# this is a array with dims snps x samples
+# genotypes are represented by single byte codes 
+# critically, as the same dims as the filters array below
 gt_array = allel.GenotypeArray(gt_dask).to_packed()
-
-# build a composite logical (mask) for the filtration criteria
-# this is array with dims snps x samples
-# note that for convenience, we are setting the mask as 1 when the genotype *fails* any filter 
-# TBD: some way to avoid this being hardcoded, i.e. dynamically build the callset[] expressions 
-filters = ((callset[chromosome + '/calldata/GQ'][:] < 30) | (callset[chromosome + '/calldata/RGQ'][:] < 30))  & (callset[chromosome + '/calldata/DP'][:] < 7)  
 
 # set all genotypes that fail filters to 'missing'
 # 239 = -1 (i.e. missing) for packed arrays
@@ -139,26 +194,7 @@ pos_array = allel.SortedIndex(callset[chromosome + '/variants/POS'])
 pos_array = pos_array[callset[chromosome + '/variants/numalt'][:] < 2]
 
 
-# In[7]:
-
-
-# calculate pi
-
-# for loop wrapper for pi calcuation
-# output looks like:
-
-# chromosome pos1 pos2 no_differences no_comparisons no_missing pi_1 pi_2 dxy
-# chromosome pos1 pos2 no_differences no_comparisons no_missing pi
-# chromosome pos1 pos2 no_differences no_comparisons no_missing dxy
-
-# check if pi output makes sense
-# - are any pis/dxys all zero?
-# - check if # missing == (pos2 - pos1)
-# - check if everything was either compared or missing
-# - write out summary of program parameters file* think about how this should work
-
-
-# In[8]:
+# In[18]:
 
 
 #Basic functions for comparing the genotypes at each site in a region: counts differences out of sites with data
@@ -201,20 +237,22 @@ def compareGTs(vec):
     return(diffs,comps,missing)
 
 
-# In[9]:
-
-
-print(max(pos_array))
-
-
 # In[19]:
 
 
 # Compute pi over a chosen window size
 
+# calculate pi
+
+# TBD:
+# - are any pis/dxys all zero?
+# - check if # missing == (pos2 - pos1)
+# - check if everything was either compared or missing
+# - write out summary of program parameters file* think about how this should work
+
 # total chromosome length
 # chr_length = max(pos_array)
-chr_length = 10000 #testing value
+chr_length = 10000 # testing value
 
 # window size:
 window_size = 1000
@@ -227,6 +265,7 @@ os.remove(args.outfile)
 outfile = open(args.outfile, 'w')
 outfile.write("chromosome" + "\t" + "window_pos_1" + "\t" + "window_pos_2" + "\t" + "avg_pi" + "\t" + "no_sites" + "\t" + "total_diffs" + "\t" + "total_comps" + "\t" + "total_missing" + "\n")
 
+# loop over windows, compute stats and write to file
 for window_pos_1 in tqdm(range (1, chr_length, window_size)):
     loc_region = pos_array.locate_range(window_pos_1, window_pos_2)
     gt_region1 = gt_array[loc_region]
@@ -234,16 +273,16 @@ for window_pos_1 in tqdm(range (1, chr_length, window_size)):
     outfile.write(str(chromosome) + "\t" + str(window_pos_1) + "\t" + str(window_pos_2) + "\t" + str(avg_pi) + "\t" + str(len(gt_region1)) + "\t" + str(total_diffs) + "\t" + str(total_comps) + "\t" + str(total_missing) + "\n")
     window_pos_2 += window_size
 
+# close output file and print complete message
 outfile.close()
-
 print("Calculations complete and written to " + args.outfile)
 
 
-# In[5]:
+# In[20]:
 
 
 # convert this notebook to a .py script
-# get_ipython().system('jupyter nbconvert --to=python pixy.ipynb')
+get_ipython().system('jupyter nbconvert --to=python pixy.ipynb')
 
 
 # In[ ]:
