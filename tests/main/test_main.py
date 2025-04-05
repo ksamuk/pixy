967ee22ee5e9058bd4f5c8e6c41a950ed2e2051d
Kieran Samuk <ksamuk@gmail.com>
2025-04-05 14:40:02 -0700

    fixed comment spacing

--- tests/main/test_main.py
+++ tests/main/test_main.py
@@ -431,9 +431,9 @@ def test_pixy_csi_index(
         generated_data_path: Path = pixy_out_dir / file
         exp_data_path: Path = expected_outputs / "baseline" / file
         assert generated_data_path.exists()
-        
-        #uncomment (and then recomment) to regenerate expected datasets
-        #shutil.copy(generated_data_path, exp_data_path)
+
+        # uncomment (and then recomment) to regenerate expected datasets
+        # shutil.copy(generated_data_path, exp_data_path)
 
         assert_files_are_consistent(generated_data_path, exp_data_path)
 
@@ -539,8 +539,8 @@ def test_pixy_main_valid_inputs(
         exp_data_path: Path = expected_outputs / "baseline" / file
         assert generated_data_path.exists()
 
-         #uncomment (and then recomment) to regenerate expected datasets
-        #shutil.copy(generated_data_path, exp_data_path)
+        # uncomment (and then recomment) to regenerate expected datasets
+        # shutil.copy(generated_data_path, exp_data_path)
         assert_files_are_consistent(generated_data_path, exp_data_path)
 
 
